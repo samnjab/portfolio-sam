e5de56d2a3010bd133329bf4358cd1c0966a43a6
samjaberi <samanjaberi.sj@gmail.com>
2023-01-29 23:44:06 -0500

    finished smooth scroll

---
+++ src/script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.home-nav').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('home').scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    })
+})
+
+document.querySelectorAll('.about-nav').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('about').scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    })
+})
+document.querySelectorAll('.projects-nav').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('projects').scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    })
+})
+document.querySelectorAll('.services-nav').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('projects').scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    })
+})
+document.querySelectorAll('.contact-nav').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('contact').scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    })
+})
+
+
